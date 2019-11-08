1023600d2fee0e9428855797abe18ea7c187c488
Maheshkumar N <pn.maheshkumar@gmail.com>
2019-11-08 09:43:22 +0530

    Add project files.

---
+++ SBReceiveQueueConApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System.Configuration;
+using System.Threading;
+
+namespace SBReceiveQueueConApp
+{
+  class Program
+  {
+    static NamespaceManager _namespaceManager;
+    static void Main(string[] args)
+    {
+      CollectSBDetails();
+      CreateQueueToRead();
+      //Console.WriteLine("Read all the Data");
+      Console.ReadKey(true);
+    }
+
+    private static void CreateQueueToRead()
+    {
+      TokenProvider tokenProvider = _namespaceManager.Settings.TokenProvider;
+      if (_namespaceManager.QueueExists("categoryqueue"))
+      {
+        //Creating the queues with details
+        //QueueDescription mnQueue = namespaceManagerClient.CreateQueue("categoryqueue");
+
+        MessagingFactory factory = MessagingFactory.Create(_namespaceManager.Address, tokenProvider);
+
+        //ReceiveAndDelete chances of not clearing the data if it crash before
+        //PeekLock, it locks the second message in line and only by calling Complete it moves to the next only then it deletes it of this is default mode
+        QueueClient catsQueueClient = factory.CreateQueueClient("categoryqueue");
+
+
+        Console.WriteLine("Receiving the Messages from the Queue....");
+        BrokeredMessage message;
+        int ctr = 1;
+        while ((message = catsQueueClient.Receive(new TimeSpan(hours: 0, minutes: 1, seconds: 5))) != null)
+        {
+          //Console.WriteLine("Message Received, Sequence: {0}, Cat: {1} and MessageID: {2}", message.SequenceNumber, message.Properties[(ctr++).ToString()], message.MessageId);
+          Console.WriteLine($"Message Received, Sequence: {message.SequenceNumber}, MessageID: {message.MessageId},\nCat: {message.Properties[(ctr++).ToString()]}");
+          message.Complete();
+          Console.WriteLine("Processing Message (sleeping).....");
+          Thread.Sleep(2000);
+        }
+        factory.Close();
+        catsQueueClient.Close();
+        _namespaceManager.DeleteQueue("categoryqueue");
+        Console.WriteLine("Finished getting all the data from the queue, Press any key to exit");
+      }
+    }
+
+    private static void CollectSBDetails()
+    {
+      _namespaceManager = NamespaceManager.CreateFromConnectionString(ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"].ToString());
+    }
+  }
+}
---
+++ SBSendQueueConApp/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.ServiceBus;
+using System.Configuration;
+
+namespace SBSendQueueConApp
+{
+  class Program
+  {
+    static NamespaceManager _namespaceManager;
+    static List<BrokeredMessage> _messageList;
+    static void Main(string[] args)
+    {
+      dynamic categories = GetJSONArray();
+      _messageList = GenerateMessages(categories);
+      Console.WriteLine($"MessageList.Count -> {_messageList.Count()}");
+      CollectSBDetails();
+      CreateQueue(_messageList.Count);
+      Console.ReadKey(true);
+    }
+
+    private static void CreateQueue(int messageCount)
+    {
+      QueueDescription catsQueue = null;
+      if (!_namespaceManager.QueueExists("categoryqueue"))
+      {
+        catsQueue = _namespaceManager.CreateQueue("categoryqueue");
+      }
+
+      MessagingFactory factory = MessagingFactory.Create(_namespaceManager.Address, _namespaceManager.Settings.TokenProvider);
+
+      QueueClient catsQueueClient = factory.CreateQueueClient("categoryqueue");
+      Console.WriteLine("Sending messages to the queueu.....");
+      for (int i = 0; i < messageCount; i++)
+      {
+        var cat = _messageList[i];
+        cat.Label = cat.Properties[(i + 1).ToString()].ToString();
+        //Console.WriteLine(cat.GetType());
+        catsQueueClient.Send(cat);
+        Console.WriteLine($"Message ID: {cat.MessageId}, Message Sent: {cat.Label}");
+      }
+    }
+
+    private static void CollectSBDetails()
+    {
+      _namespaceManager = NamespaceManager.CreateFromConnectionString(ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"].ToString());
+    }
+
+    private static List<BrokeredMessage> GenerateMessages(dynamic categories)
+    {
+      List<BrokeredMessage> result = new List<BrokeredMessage>();
+      //int ctr = 0;
+      foreach (var item in categories)
+      {
+        //Console.WriteLine($"{item.CategoryID} -> {item}");
+        BrokeredMessage message = new BrokeredMessage();
+        message.Properties.Add(item.CategoryID.ToString(), item.ToString());
+        result.Add(message);
+        //Console.WriteLine(result[ctr++]);
+      }
+      return result;
+    }
+
+    private static JArray GetJSONArray()
+    {
+      string jsonData = null;
+      using (StreamReader reader = new StreamReader(@"../../Categories.txt"))
+      {
+        jsonData = reader.ReadToEnd();
+      }
+      JArray jArrayData = JArray.Parse(jsonData);
+      return jArrayData;
+    }
+  }
+}
